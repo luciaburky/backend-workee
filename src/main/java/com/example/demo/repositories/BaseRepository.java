628247434024eba20104e851bc0a9dbad2d041e2
camicitro <camilacitro1106@gmail.com>
2025-07-04 18:17:17 -0300

    Creación de carpetas y clases de Base

---
+++ src/main/java/com/example/demo/repositories/BaseRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repositories;
+import com.example.demo.entities.Base;
+import java.io.Serializable;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository <E extends Base, ID extends Serializable> extends JpaRepository<E, ID>{
+    
+}
---
+++ src/main/java/com/example/demo/services/BaseService.java
@@ -0,0 +1,15 @@
+package com.example.demo.services;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.example.demo.entities.Base;
+
+public interface BaseService <E extends Base, ID extends Serializable>{
+    public E findById(ID id) throws Exception;
+    public List<E> findAll() throws Exception;
+    public E save(E entity) throws Exception;
+    public E update (ID id, E entity) throws Exception;
+    public boolean delete(ID id) throws Exception;
+
+}
