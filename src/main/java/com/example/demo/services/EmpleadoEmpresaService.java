f77cd7cda79eb96904bfc07d7e06663c04ab402f
Camila Citro <97923424+camicitro@users.noreply.github.com>
2025-07-10 17:48:27 -0300

    Deje vacio el service

--- src/main/java/com/example/demo/services/EmpleadoEmpresaService.java
+++ src/main/java/com/example/demo/services/EmpleadoEmpresaService.java
@@ -5,9 +5,5 @@ import com.example.demo.dtos.ModificarEmpleadoEmpresaDTO;
 import com.example.demo.entities.EmpleadoEmpresa;
 
 public interface EmpleadoEmpresaService extends BaseService<EmpleadoEmpresa, Long>{
-    public EmpleadoEmpresa crearEmpleadoEmpresa(CrearEmpleadoEmpresaDTO empleadoEmpresaRequestDTO) throws Exception;
-    public EmpleadoEmpresa actualizarEmpleado(ModificarEmpleadoEmpresaDTO modificarEmpleadoEmpresaDTO, Long idEmpleadoEmpresa) throws Exception;
-
-    public Long contarEmpleadosActivos(Long idEmpresa) throws Exception;
-    public Boolean darDeBajaEmpleado(Long idEmpleadoEmpresa) throws Exception;
+    
 }
