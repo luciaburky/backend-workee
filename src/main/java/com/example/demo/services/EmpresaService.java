a28f9a6909e6e43425998f25c02d836b98ef2bf9
Camila Citro <97923424+camicitro@users.noreply.github.com>
2025-07-10 17:48:55 -0300

    Deje vacio el service

--- src/main/java/com/example/demo/services/EmpresaService.java
+++ src/main/java/com/example/demo/services/EmpresaService.java
@@ -4,7 +4,7 @@ import com.example.demo.dtos.ModificarEmpresaDTO;
 import com.example.demo.entities.Empresa;
 
 public interface EmpresaService extends BaseService<Empresa, Long>{
-    public Empresa modificarEmpresa(ModificarEmpresaDTO modificarEmpresaDTO, Long id) throws Exception;
+   
 }
 
 
