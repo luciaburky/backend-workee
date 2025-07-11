7a02f2625dbc1df7bcf0efa2a2b7565f55e7f281
camicitro <camilacitro1106@gmail.com>
2025-07-10 22:18:37 -0300

    Agregado de orden al traer paises

--- src/main/java/com/example/demo/repositories/params/PaisRepository.java
+++ src/main/java/com/example/demo/repositories/params/PaisRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repositories.params;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.params.Pais;
@@ -9,5 +11,14 @@ import com.example.demo.repositories.BaseRepository;
 
 @Repository
 public interface PaisRepository extends BaseRepository<Pais, Long> {
+
+    @Query(value = "SELECT * FROM pais WHERE fecha_hora_baja IS NULL " + 
+            "ORDER BY nombre_pais ASC", 
+            nativeQuery = true
+            )
+    List<Pais> buscarPaisesActivos();
+
     Optional<Pais> findByNombrePaisIgnoreCase(String nombrePais);
+
+    List<Pais> findAllByOrderByNombrePaisAsc();
 }
