b3cf5a28e99afd71d51464f3f0f9a3c995cba368
camicitro <camilacitro1106@gmail.com>
2025-07-14 19:52:21 -0300

    Agregué en la query de filtrar empresas que traiga solo las activas

--- src/main/java/com/example/demo/repositories/empresa/EmpresaRepository.java
+++ src/main/java/com/example/demo/repositories/empresa/EmpresaRepository.java
@@ -15,8 +15,8 @@ public interface EmpresaRepository extends BaseRepository<Empresa, Long>  {
     @Query(
         value = "SELECT * FROM empresa " + 
                 "WHERE (:idsRubros IS NULL OR id_rubro IN (:idsRubros)) " + 
-                "AND (:idsProvincias IS NULL OR id_provincia IN (:idsProvincias)) " //TODO: Agregar el filtro de ofertas
-                ,                                                                 //DUDA: deberia agregar un filtro por nombre aca? o eso lo hacemos desde el front como hicieron en parametros?
+                "AND (:idsProvincias IS NULL OR id_provincia IN (:idsProvincias)) " + //TODO: Agregar el filtro de ofertas
+                "AND fecha_hora_baja IS NULL",                                                                 //DUDA: deberia agregar un filtro por nombre aca? o eso lo hacemos desde el front como hicieron en parametros?
         nativeQuery = true
     )
     public List<Empresa> buscarEmpresasConFiltros(@Param("idsRubros") List<Long> idsRubros, @Param("idsProvincias") List<Long> idsProvincias);
