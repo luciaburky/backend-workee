8527ddfa708c12fc03d44892099f9f2b035c039d
camicitro <camilacitro1106@gmail.com>
2025-08-26 08:19:54 -0300

    Creacion de clases relacionadas a postulaciones

---
+++ src/main/java/com/example/demo/services/postulaciones/PostulacionOfertaServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.demo.services.postulaciones;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostulacionOfertaServiceImpl implements PostulacionOfertaService{
+
+}
