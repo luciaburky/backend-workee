0b4b05a5a55775b6eda72ffbec75b44a479e3e77
julil <julilorenzo4467@gmail.com>
2025-09-07 23:14:54 -0300

    Arreglo Candidato: Traer CandidatoHabilidad con fechaHoraBaja null

--- src/main/java/com/example/demo/repositories/candidato/CandidatoRepository.java
+++ src/main/java/com/example/demo/repositories/candidato/CandidatoRepository.java
@@ -13,7 +13,15 @@ public interface CandidatoRepository extends BaseRepository<Candidato, Long> {
     
     List<Candidato> findAllByOrderByNombreCandidatoAsc();
     
-    @Query("SELECT c FROM Candidato c LEFT JOIN FETCH c.habilidades ch LEFT JOIN FETCH ch.habilidad WHERE c.id = :id")
+    //@Query("SELECT c FROM Candidato c LEFT JOIN FETCH c.habilidades ch LEFT JOIN FETCH ch.habilidad WHERE c.id = :id")
+    @Query("""
+        SELECT DISTINCT c
+        FROM Candidato c
+        LEFT JOIN FETCH c.habilidades ch
+        LEFT JOIN FETCH ch.habilidad h
+        LEFT JOIN FETCH h.tipoHabilidad
+        WHERE c.id = :id
+        """)
     Optional<Candidato> findByIdWithHabilidades(@Param("id") Long id);
 
     boolean existsByGeneroIdAndFechaHoraBajaIsNull(Long generoId);
