6eb94d7ea598e822908adcdf87363e5b4a980a88
camicitro <camilacitro1106@gmail.com>
2025-09-25 15:16:58 -0300

    Agrego para validar fechas (SIN TERMINAR)

--- src/main/java/com/example/demo/services/metricas/MetricasServiceImpl.java
+++ src/main/java/com/example/demo/services/metricas/MetricasServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exceptions.EntityNotValidException;
 import com.example.demo.repositories.seguridad.UsuarioRepository;
 
 @Service
@@ -23,6 +24,32 @@ public class MetricasServiceImpl implements MetricasService{
 
 
     private Boolean validacionFiltrosCorrectos(Date fechaDesde, Date fechaHasta){
-        return true;
+        if(fechaDesde != null){
+            if(fechaHasta != null){
+                if(fechaDesde.after(fechaHasta)){
+                    throw new EntityNotValidException("No es posible ingresar una fecha desde mayor a la fecha hasta");
+                }
+            }
+            if(fechaDesde.after(new Date())){
+                throw new EntityNotValidException("No es posible ingresar una fecha desde mayor a la fecha actual");
+            }
+        }
+        
+        
+        return true;//TODO: cambiarlo
     }
 }
+
+
+/*
+ * 
+ * No se si de esto tengo que hacer algo o si es puramente del front. Pero básicamente se tiene una fecha desde y una fecha hasta. 
+
+Pero para lo del back:
+
+si no selecciona la fecha desde, entonces trae toda la información hasta la fecha indicada. 
+
+Si no selecciona la fecha hasta entonces trae toda la información desde la fecha indicada hasta la fecha actual. 
+
+Si selecciona ambas entonces trae dentro de ese rango de fechas.
+ */
