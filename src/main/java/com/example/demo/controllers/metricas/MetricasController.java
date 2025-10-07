0e3289f586f2d99247cf952fa0a064d1e379c399
camicitro <camilacitro1106@gmail.com>
2025-10-07 14:43:58 -0300

    Correccion endpoint metricas de la empresa (nombre de swagger)

--- src/main/java/com/example/demo/controllers/metricas/MetricasController.java
+++ src/main/java/com/example/demo/controllers/metricas/MetricasController.java
@@ -46,7 +46,7 @@ public class MetricasController {
     }
 
     //EMPRESAS
-    @Operation(summary = "EMPRESA: Ver proporción de géneros postulados")
+    @Operation(summary = "Ver métricas de una empresa")
     @PutMapping("/empresa/{idEmpresa}")
     @PreAuthorize("hasAuthority('METRICAS_EMPRESA')")
     public ResponseEntity<?> verDistribucionGeneros(@PathVariable Long idEmpresa, @RequestBody FiltroFechasDTO filtroFechasDTO) {
